20ae5d0c95b47768cc008676f3c36effbeded289
JenniferThompson2432455 <JenniferThompson2432455@cdzhongguan.com>
2026-04-09 18:56:17 +0800

    Create Ownable.sol

---
+++ Ownable.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Ownable {
+    address public owner;
+
+    event OwnershipTransferred(address indexed previous, address indexed newOwner);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    function transferOwnership(address newOwner) external onlyOwner {
+        require(newOwner != address(0), "Zero address");
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
+}
